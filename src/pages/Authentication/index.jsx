0ff836b86048cc9127bc7e95ba99e8fac95b2b05
Ghiwa El Daccache <ghiwadaccache4@gmail.com>
2024-03-24 08:51:26 +0200

    added-signup-link

--- src/pages/Authentication/index.jsx
+++ src/pages/Authentication/index.jsx
@@ -1,18 +1,22 @@
 import "./style.css";
 
 // import axios from "axios";
-// import { useEffect, useState } from "react";
-// import { useNavigate } from "react-router-dom";
+import {useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 
 const Authentication = () => {
-    let IsLogin = false; 
+    const [isLogin, setIsLogin] = useState(false);
+    const navigate = useNavigate();
+
     return (
     <div className="flex page">
-            {/* <div className="bg-img"></div>
+             <div className="bg-img"></div>
             <div className="flex center">
-                <div className="flex column center login-form gap primary-bg">
-                    <h2>Welcome back</h2>
+                
+                {isLogin ? (
+                    <div className="flex column center login-form gap primary-bg">
+                   <h2>Welcome back</h2>
                     <input
                         className="f-width input-style"
                         type="text"
@@ -25,22 +29,25 @@ const Authentication = () => {
                         placeholder="Password"
                     />
 
-                    <button
-                        className="btn-style f-width">
+                    <button className="btn-style f-width" onClick={ () => {
+                        navigate("/Home")
+                    }
+
+                    }>
                         Login
                     </button>
 
                     <small>
-                        Don't have an account? <span><b>Sign Up</b></span>
+                        Don't have an account? <span onClick={() => {
+                            setIsLogin(false);
+                        }}>
+                            <b>Sign Up</b></span>
                     </small>
+                    </div> 
+                ) : (
 
-                    </div>
-            </div> */}
-
-        <div className="bg-img"></div>
-            <div className="flex center">
-                <div className="flex column center login-form gap primary-bg">
-                    <h2>Welcome</h2>
+                    <div className="flex column center login-form gap primary-bg">
+                     <h2>Welcome</h2>
                     <input
                         className="f-width input-style"
                         type="text"
@@ -65,12 +72,14 @@ const Authentication = () => {
                     </button>
 
                     <small>
-                        Already have an account? <span><b>Login</b></span>
+                        Already have an account? <span onClick={() => {
+                            setIsLogin(true);
+                        }}><b>Login</b></span>
                     </small>
-
-                    </div>
-            </div>
-
+                </div>
+                    )}
+                
+        </div>
     </div>
 
   );
---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,11 @@
+import "./style.css";
+
+const Home = () =>{
+    return (
+        <div className="flex page">
+            <h2>test</h2>
+        </div>
+    )
+}
+
+export default Home;
