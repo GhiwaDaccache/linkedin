423a3742ff87f78443e8eb529abf6ea05f2abce7
Ghiwa El Daccache <ghiwadaccache4@gmail.com>
2024-03-25 21:38:06 +0200

    added-get-posts

--- Backend/get_posts.php
+++ Backend/get_posts.php
@@ -1,29 +1,34 @@
 <?php
 include('connection.php');
 
-$url_param = $_GET['user_id'];
+$user_id = $_GET['user_id'];
 
 $load_posts = $mysqli->prepare(
-'select * 
-from posts p 
-left join user_followers u on u.following_id=p.user_id 
+'select p.description, p.title, p.date, users.name, companies.name
+from posts p
+left join user_followers u on u.following_id=p.user_id
 left join compa_followers c on c.company_id=p.company_id
-where u.follower_id =? or c.follower_id = ?');
+left join users on users.id=p.user_id
+left join companies on companies.id=p.company_id
+where u.follower_id =? or c.follower_id = ?
+');
 
 
-$load_posts->bind_param('s', $url_param);
+$load_posts->bind_param('ii', $user_id, $user_id);
 $load_posts->execute();
 $load_posts->store_result();
 $num_rows = $load_posts->num_rows();
 
 $posts = [];
-// $load_posts->bind_result($id, $description, $status, $user_id, $category_id);
+$load_posts->bind_result($description, $title, $date, $user_name, $company_name);
+
 while ($load_posts->fetch()) {
     $post = [
-        // 'id' => $id,
-        // 'description' => $description,
-        // 'status' => $status,
-        // 'category_id' => $category_id,
+        'description' => $description,
+        'title' => $title,
+        'date' => $date,
+        'user_name' => $user_name,
+        'company_name' => $company_name,
     ];
     $posts[] = $post;
 }
